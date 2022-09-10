856d492e8b735bad6aba59190a0d21134e514b16
Vikas-744 <69007962+Vikas-744@users.noreply.github.com>
2022-09-10 18:57:57 +0530

    build

---
+++ precache-manifest.4800c2c7b62713f774ddf3726ee47c99.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e566e9c6447e6655f387acd932a250de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3899008e746e69a61af",
+    "url": "/static/css/3.39934f7b.chunk.css"
+  },
+  {
+    "revision": "5b16f431ef4c8d7cb23b",
+    "url": "/static/css/4.521d0ad9.chunk.css"
+  },
+  {
+    "revision": "f8c234e555bb8149d85a",
+    "url": "/static/css/main.08ad1b39.chunk.css"
+  },
+  {
+    "revision": "fb7c78127b89c94cfe23",
+    "url": "/static/js/2.95ee872d.chunk.js"
+  },
+  {
+    "revision": "bc236b351092d334844238e6858d7051",
+    "url": "/static/js/2.95ee872d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3899008e746e69a61af",
+    "url": "/static/js/3.9bbce758.chunk.js"
+  },
+  {
+    "revision": "5b16f431ef4c8d7cb23b",
+    "url": "/static/js/4.93878ccb.chunk.js"
+  },
+  {
+    "revision": "f8c234e555bb8149d85a",
+    "url": "/static/js/main.9df7a2fc.chunk.js"
+  },
+  {
+    "revision": "b65fca23c24e0aa6f4db",
+    "url": "/static/js/runtime-main.e48ab3bf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "522bb625ff35c36301278058565df077",
+    "url": "/static/media/IISc_Logo.522bb625.jpg"
+  },
+  {
+    "revision": "5a305d7125e486422a74298e6de62408",
+    "url": "/static/media/Matlab_Logo.5a305d71.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8e388bc4f9fa3177192785367d9fcb4a",
+    "url": "/static/media/NPTEL square.8e388bc4.jpg"
+  },
+  {
+    "revision": "90de3a7a582507079fd7c0d3b52dcf7c",
+    "url": "/static/media/completed.90de3a7a.png"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/developerActivity.c1a95f73.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/manOnTable.34409398.svg"
+  },
+  {
+    "revision": "0db39ac71db261b41fa464e23db25fa7",
+    "url": "/static/media/search-paper.0db39ac7.png"
+  },
+  {
+    "revision": "3510799b586825f1863ca661d1f23211",
+    "url": "/static/media/sistLogo.3510799b.png"
+  },
+  {
+    "revision": "23d1479de239aa369e90a0a9d94b5c43",
+    "url": "/static/media/trophy.23d1479d.png"
+  }
+]);
